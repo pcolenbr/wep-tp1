aea463b5bc7a2d66b5d9872b4f5c6a1905bbf50a
Pedro Colen Cardoso <pedro@dreamt.com>
2014-11-18 18:35:37 -0200

    Projetos

--- projetos.php
+++ projetos.php
@@ -6,23 +6,19 @@
 		<div id="dt_carousel" class="carousel slide dt_carousel">
 				<div class="carousel-inner dt_carousel_inner">
 					<div class="dt_carousel_item item active">
-						<img class="img-responsive" src="<?php echo $site_url?>/images/350x400.gif" alt="Banner 1">
+						<img class="img-responsive" src="<?php echo $site_url?>/images/draemtechpng.png" alt="Banner 1">
 					</div>
 
 					<div class="dt_carousel_item item">
-						<img class="img-responsive" src="<?php echo $site_url?>/images/350x400(1).gif" alt="Banner 2">
+						<img class="img-responsive" src="<?php echo $site_url?>/images/gestech.png" alt="Banner 2">
 					</div>
 
 					<div class="dt_carousel_item item">
-						<img class="img-responsive" src="<?php echo $site_url?>/images/350x400(2).gif" alt="Banner 3">
+						<img class="img-responsive" src="<?php echo $site_url?>/images/iniciacao.png" alt="Banner 3">
 					</div>
 
 					<div class="dt_carousel_item item">
-						<img class="img-responsive" src="<?php echo $site_url?>/images/350x400(3).gif" alt="Banner 3">
-					</div>
-
-					<div class="dt_carousel_item item">
-						<img class="img-responsive" src="<?php echo $site_url?>/images/350x400(4).gif" alt="Banner 3">
+						<img class="img-responsive" src="<?php echo $site_url?>/images/monitoria.png" alt="Banner 3">
 					</div>
 				</div>
 				<a href="#dt_carousel" class="left carousel-control  dt_carousel_control dt_carousel_controle_left" data-slide="prev"><img alt="Carousel Dreamtech Left Arrow" src="<?php echo $site_url?>/images/arrow_left.png"></a>
